1c1002e459032e07a0a7af602a43087f08120619
Manon <manon.beurtey@gmail.com>
2024-05-09 15:02:01 +0200

    lastlast

--- code.py
+++ code.py
@@ -14,7 +14,7 @@ instructions = expyriment.stimuli.TextScreen("Instructions",
 instructions_break = expyriment.stimuli.TextScreen("Break", 
                                              " You can take a break. Press ENTER to go back to the experiment ",text_font= "Monospace")
 
-exp.data_variable_names = ["Cue","SOA","target","key","response time"]
+exp.data_variable_names = ["Cue","SOA","Target","Key","RT", "ITI"]
 # Fixation crosses
 cross = expyriment.stimuli.FixCross(size=(50, 50), line_width=4, colour=expyriment.misc.constants.C_WHITE) # Original cross
 cross.preload()
@@ -23,26 +23,23 @@ cross_red = expyriment.stimuli.FixCross(size=(50, 50), line_width=4, colour=expy
 cross_red.preload()
 
 # BLOCKS
-spectral_expectation = ["fixed_spectral", "variable_spectral"]
 spectral_expectation_blocks = ["fixed_spectral", "variable_spectral"]*6
 random.shuffle(spectral_expectation_blocks) # 12 blocks counterbalanced
 
 # TRIALS
-temporal_expectation = ["fixed_SOA","variable_SOA"]
 temporal_expectation_trials = ["fixed_SOA","variable_SOA"]*50
 
 # Logarithmic steps for low expectations conditions (SOA and target)
-def steps(start_range1, stop_range1, start_range2, stop_range2, number):
+def steps(start_range1, stop_range1, start_range2, stop_range2, number1, number2):
     start1 = numpy.log10(start_range1)
     end1 = numpy.log10(stop_range1)
     start2 = numpy.log10(start_range2)
     end2 = numpy.log10(stop_range2)
-    ranged_int_list = list(numpy.int_(numpy.concatenate((numpy.logspace(start1, end1, num=number), numpy.logspace(start2, end2, num=number)))))
+    ranged_int_list = list(numpy.int_(numpy.concatenate((numpy.logspace(start1, end1, num=number1), numpy.logspace(start2, end2, num=number2)))))
     return ranged_int_list
 
 # Variables
 N = 100 # number of trials
-ITI = random.randint(1250, 1500) # Intertrial duration
 target_duration = 50 
 cue_duration = 250
 
@@ -50,32 +47,30 @@ high_cue_freq = 1318 # Cues frequencies
 low_cue_freq = 1046
 
 fixed_SOA = 1250 # SOA duration
-variable_SOA = steps(350, 950, 1550, 2150, 3)
+variable_SOA = steps(350, 950, 1550, 2150, 3, 2) # 5 steps in total
 
 fixed_target_freq = 1975 # Targets frequencies
-variable_target_freq = steps(1725, 1925, 2025, 2225, 5)
+variable_target_freq = steps(1725, 1925, 2025, 2225, 5, 5) # 10 steps in total
 
 # Lists variable SOA and variable targets
-variable_target_list = variable_target_freq * (N//2)
+variable_target_list = variable_target_freq * (N//10)
 random.shuffle(variable_target_list)
 
-variable_SOA_list = variable_SOA * (N//2)
+variable_SOA_list = variable_SOA * (N//20)
 random.shuffle(variable_SOA_list)
 
 # Functions
-def get_trial_parameters(spectral, temporal):
+def get_trial_parameters(spectral, temporal, trial_index):
     if temporal == "fixed_SOA":
         cue_freq = high_cue_freq
         SOA = fixed_SOA
     else:  # variable_SOA
         cue_freq = low_cue_freq
-        for var in variable_SOA_list:
-            SOA = var
+        SOA = variable_SOA_list[trial_index % len(variable_SOA_list)]
     if spectral == "fixed_spectral":
         target_freq = fixed_target_freq
     else: # variable spectral
-        for tar in variable_target_list:
-            target_freq = tar
+        target_freq = variable_target_list[trial_index % len(variable_target_list)]
     return cue_freq, SOA, target_freq
 
 def run_trial(cue_freq, SOA, target_freq):
@@ -95,9 +90,10 @@ def run_trial(cue_freq, SOA, target_freq):
         exp.clock.wait(100)  
         cross.present()
 
+    ITI = random.randint(1250, 1500) # Intertrial duration
     exp.clock.wait(ITI)
     cross.present()
-    return key, rt
+    return key, rt, ITI
 
 # Running experiment
 expyriment.control.start(skip_ready_screen=True)
@@ -110,10 +106,10 @@ for i, block in enumerate(spectral_expectation_blocks):
     random.shuffle(temporal_expectation_trials)
     for j, trial in enumerate(temporal_expectation_trials):
         temporal_expectation_value = trial
-        cue_freq, SOA, target_freq  = get_trial_parameters(spectral_expectation_value, temporal_expectation_value)
-        key, rt = run_trial(cue_freq, SOA, target_freq)
+        cue_freq, SOA, target_freq  = get_trial_parameters(spectral_expectation_value, temporal_expectation_value, j)
+        key, rt, ITI = run_trial(cue_freq, SOA, target_freq)
         print(i,j)
-        exp.data.add([cue_freq, SOA, target_freq, key, rt])
+        exp.data.add([cue_freq, SOA, target_freq, key, rt, ITI])
 
         if j == N:
             instructions_break.present()        
